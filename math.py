297e1dbc3bab58d69acea7626fa93fc8a2ed45f6
Alias Lyend <Lyend.chatango@gmail.com>
2016-09-11 22:54:35 -0500

    Update math.py
    Padded with some more protection

--- math.py
+++ math.py
@@ -1,5 +1,11 @@
 from sys import argv
+from string import punctuation, digits
 try:
-  print(eval(argv[1]))
-except Exception:
+  complete = ''
+  for x in argv[1]:
+    if x in punctuation or x in digits:
+      complete += x
+  print(eval(complete))
+except IndexError:
   print('Not enough args')
+except Exception: pass
