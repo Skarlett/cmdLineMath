b9108863216b39a06d62bd1b481af627b84ae5e3
Alias Lyend <Lyend.chatango@gmail.com>
2016-09-11 23:58:02 -0500

    Update math.py

--- math.py
+++ math.py
@@ -5,7 +5,12 @@ try:
   for x in argv[1]:
     if x in punctuation or x in digits:
       complete += x
-  print(eval(complete))
+  data = float(eval(complete))
+  if int(str(data).split('.')[1]) == 0:
+    print(int(data))
+  else:
+    print(data)
 except IndexError:
   print('Not enough args')
 except Exception: pass
+
