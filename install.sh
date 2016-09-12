222ef074af202264437bed69b674d0b716f9ee15
Alias Lyend <Lyend.chatango@gmail.com>
2016-09-11 23:16:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
-chmod +x math.py && chmod +x math && mv math /bin && mv math.py /usr/local/src && rm install.sh
-echo "Done."
+chmod +x math.py && chmod +x math && mv math /bin && mv math.py /usr/local/src && rm install.sh && echo "Done."
