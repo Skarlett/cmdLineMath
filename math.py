88fa7fe0b2e3a0a6a81c4d25da12180ec2d2aeaa
Skarlett <Skarlett@users.noreply.github.com>
2017-12-23 15:20:46 -0600

    Update math.py
    Cleaned up.

--- math.py
+++ math.py
@@ -1,16 +1,9 @@
+#!/usr/bin/python
 from sys import argv
-from string import punctuation, digits
-try:
-  complete = ''
-  for x in argv[1]:
-    if x in punctuation or x in digits:
-      complete += x
-  data = float(eval(complete))
-  if int(str(data).split('.')[1]) == 0:
-    print(int(data))
-  else:
-    print(data)
-except IndexError:
-  print('Not enough args')
-except Exception: pass
+
+def safe_eval(string, d={"__builtins__":None}):
+  return eval(string, d)
+
+if __name__ == '__main__':
+  print(eval(' '.join(argv[1:])))
 
